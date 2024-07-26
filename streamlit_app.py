2207c6504bdc6d2de1a699bf7491ffd1ae49baad
jan.pelisek <jan.pelisek@epptec.eu>
2024-07-26 11:20:43 +0200

    Added functionaly to submit button - Test

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,13 +18,15 @@ if st.session_state.get("data") is None:
 
 df = st.session_state["data"]
 
+def Add_new_record():
+    st.write(name, hire_date, salary, fte, type, email)
 
 with st.form("Worker"):
     st.write("Worker data entry form")
     name = st.text_input("Name:")
-    hire_date = st.text_input("Hire Date:")
-    salary = st.number_input("Salary:")
-    FTE = st.number_input("FTE:")
+    hire_date = st.date_input_input("Hire Date:",format="DD/MM/YYYY")
+    salary = st.number_input("Salary:", min_value=0)
+    fte = st.number_input("FTE:",min_value=0, max_value=1.4, format="%0.1f")
     type = st.selectbox(
         "Type:",
         ["IČO", "Full-time", "Part-Time"]
@@ -32,12 +34,13 @@ with st.form("Worker"):
     email = st.text_input("Email:")
     col1, col2, col3, col4 = st.columns(4)
     with col1:
-        new_record = st.form_submit_button("New")
+        new_record = st.form_submit_button("New",on_click=Add_new_record )
     with col2:
         search_record = st.form_submit_button("Search")
     with col3:
-        update_record = st.submit_button("Update")
+        update_record = st.form_submit_button("Update")
     with col4:
-        clear_form = st.submit_button("Clear")
+        clear_form = st.form_submit_button("Clear")
+
 
 st.dataframe(df)
